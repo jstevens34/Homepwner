bae6ae07a01a4091cdee410cf401bfb4f4c6a049
Joe Stevens <joseph.stevens34@gmail.com>
2016-07-24 01:23:38 -0400

    Setup table view controller and view

---
+++ Homepwner/ItemsViewsController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewsController.swift
+//  Homepwner
+//
+//  Created by Joe Stevens on 7/24/16.
+//  Copyright © 2016 Joe Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}

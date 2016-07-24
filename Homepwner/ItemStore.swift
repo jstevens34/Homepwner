22b96b4661a1f123ad3081c5c837fdf93a2b04a5
Joe Stevens <joseph.stevens34@gmail.com>
2016-07-24 14:04:13 -0400

    Added create item to ItemStore

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,20 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Joe Stevens on 7/24/16.
+//  Copyright © 2016 Joe Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    func createItem() -> Item{
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        return newItem
+    }
+}
